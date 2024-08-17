fae99f5838dfb2b30e44988d953cfa6181bfa1d0
Subhankar Naskar <subhnaskar11@gmail.com>
2024-08-17 19:07:04 +0530

    fixed bugs -subhankar

--- app.js
+++ app.js
@@ -32,59 +32,104 @@ app.use(
 app.use(express.json())
 app.use('/api/v1', basics)
 const rooms = {}
-io.on('connection', (socket) => {
-  socket.on('joinGroup', (m) => {
-    if (!rooms[m.roomID]) {
-      rooms[m.roomID] = {
-        creator: socket.id,
-      }
-      console.log('new owner created')
-      console.log(rooms)
-      socket.join(m.roomID)
-    } else {
-      console.log('targeting owner')
-      console.log(rooms[m.roomID])
-      io.to(rooms[m.roomID].creator).emit('allowPermission',socket.id)
-      io.to(rooms[m.roomID].creator).emit('getCurrData')
+function addUserToRoom(socket, userData, roomID, idx) {
+  if (rooms[idx]) {
+    rooms[idx].users.push({ socketID: socket.id, userData, roomID: roomID })
+  }
+}
+function removeUserFromRoom(socket, roomID) {
+  if (rooms[roomID]) {
+    const index = rooms[roomID].users.findIndex(
+      (user) => user.socketID === socket.id
+    )
+
+    if (index !== -1) {
+      rooms[roomID].users.splice(index, 1)
     }
 
-    console.log('joined')
-    console.log(m)
-    socket.join(m.roomID)
+    if (
+      rooms[roomID].creator === socket.id ||
+      rooms[roomID].users.length === 0
+    ) {
+      delete rooms[roomID]
+    }
+  }
+}
+io.on('connection', (socket) => {
+  socket.on('joinGroup', (m) => {
+    // if (!rooms[m.userData.user.clerkId]) {
+    //   rooms[m.userData.user.clerkId] = {
+    //     creator: socket.id,
+    //   }
+    //   console.log('new owner created')
+    //   console.log(rooms)
+    //   socket.join(m.roomID)
+    // } else {
+    //   console.log('targeting owner')
+    //   console.log(rooms[m.roomID])
+    //   io.to(rooms[m.roomID].creator).emit('allowPermission',socket.id)
+    //   io.to(rooms[m.roomID].creator).emit('getCurrData')
+    // }
 
-    const room = io.sockets.adapter.rooms.get(m.roomID)
-    const numClients = room ? room.size : 0
-    console.log(numClients)
-    socket.to(m.roomID).emit('joinGroup', { length: numClients })
+    // console.log('joined')
+    // console.log(m)
+    // socket.join(m.roomID)
+
+    // const room = io.sockets.adapter.rooms.get(m.roomID)
+    // const numClients = room ? room.size : 0
+    // console.log(numClients)
+    // socket.to(m.roomID).emit('joinGroup', { length: numClients, userData:m.userData })
+    socket.roomID = m.roomID
+
+    let idx = m.roomID
+    if (!rooms[idx]) {
+      rooms[idx] = {
+        creator: { clrkID: m.userData.user.clerkId, socketID: socket.id },
+        users: [
+          { socketID: socket.id, userData: m.userData, roomID: m.roomID },
+        ],
+      }
+    } else {
+      addUserToRoom(socket, m.userData, m.roomID, idx)
+      io.to(rooms[idx].creator.socketID).emit('getCurrData')
+    }
+    socket.join(m.roomID)
+    io.to(m.roomID).emit('joinGroup', {
+      length: rooms[idx].users.length,
+      userData: rooms[idx],
+    })
+    console.log(rooms)
   })
   // socket.on('getCurrData',()=>{
 
   // })
   socket.on('generateRoomRequest', (m) => {
-    console.log('generating room id')
     socket.emit('generateRoomRequest', { roomID: socket.id })
   })
   socket.on('sendSignal', (m) => {
-    console.log(m)
     const room = io.sockets.adapter.rooms.get(m.roomID)
     const numClients = room ? room.size : 0
-    console.log(`curr room id ${m.roomID} -- ${numClients}`)
-    socket
-      .to(m.roomID)
-      .emit('getResponse', {
-        value: m.value,
-        position: m.position,
-        id: socket.id,
-      })
+    socket.to(m.roomID).emit('getResponse', {
+      value: m.value,
+      position: m.position,
+      id: socket.id,
+      userData: m.userData,
+    })
   })
   socket.on('disconnect', () => {
-    // If the creator leaves, remove the room's entry or handle it accordingly
-    for (const roomID in rooms) {
-      if (rooms[roomID].creator === socket.id) {
-        delete rooms[roomID] // You might want to handle this differently
-        console.log(`Creator left, room ${roomID} is now without a creator.`)
-      }
+    const roomID = socket.roomID
+    console.log(roomID)
+    removeUserFromRoom(socket, roomID)
+
+    if(rooms[roomID]){
+      io.to(roomID).emit('joinGroup', {
+        length: rooms[roomID].users.length,
+        userData: rooms[roomID],
+      })
     }
+    console.log('leaving')
+    console.log(roomID)
+    console.log(rooms)
   })
 })
 
